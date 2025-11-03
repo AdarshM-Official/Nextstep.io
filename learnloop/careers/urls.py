e971e8f5e2c3046a6557d317ee9ab932a975a29d
AdarshM-Official <adarshm.off@gmail.com>
2025-11-03 14:54:40 +0530

    reverse ml

--- learnloop/careers/urls.py
+++ learnloop/careers/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.careers_view, name='careers'), #Careers main page
-    path('predict/', views.career_predict, name='career_predict'), #Career prediction form
 ]
